d9b2a5920b26eb4d4592ba9a4f05e84b720a54e7
JREC <jrenriquez@ufm.edu>
2015-08-23 07:34:34 -0600

    Final Cached Function
    The final submission of my assignment.

--- cachematrix.R
+++ cachematrix.R
@@ -1,36 +1,41 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This function really is a list of 4 functions to operate cached matrix.
+## one function is to set values, other to get values for both (cached and inverse matrix)
 
 makeCacheMatrix <- function(x = matrix()) {
-  m<-NULL
+  mat<-NULL ##this initialize mat
   set<-function(y){
-    x<<-y
-    m<<-NULL
+    x<<-y ##this gives x a value in the parent enviroment. 
+    mat<<-NULL ##this gives mat a value in the parent environment. 
   }
-  get<-function() x
-  setmatrix<-function(solve) m<<- solve
-  getmatrix<-function() m
+  get<-function() x 
+  setmatrix<-function(solve) mat<<- solve
+  getmatrix<-function() mat
   list(set=set, get=get,
        setmatrix=setmatrix,
        getmatrix=getmatrix)
 }
 
 
-## Write a short comment describing this function
+## this function creates a inverse of a "cached" Matrix. If this calculation
+## has been done before it gives the cached answer otherwise it calculates it. 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-  m<-x$getmatrix()
-  if(!is.null(m)){
+  mat<-x$getmatrix()
+  ##the next if checks if the matrix has been calculated before to skip calculation
+  if(!is.null(mat)){
     message("getting cached data")
-    return(m)
+    return(mat)
   }
+  #if the inverse of x has not been calculated it calculated now. 
   matrix<-x$get()
-  m<-solve(matrix, ...)
-  x$setmatrix(m)
-  m
+  mat<-solve(matrix, ...)
+  #this makes sure to save the matrix in the cache. 
+  x$setmatrix(mat)
+  mat
 }
 
 
